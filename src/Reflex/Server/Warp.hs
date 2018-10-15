206c3aba9a85bc2409e64c78cd941b528ed9c590
Isaac Elliott <isaace71295@gmail.com>
2018-10-15 16:08:49 +1000

    Added simple entrypoint

---
+++ src/Reflex/Server/Warp.hs
@@ -0,0 +1,22 @@
+{-# language FlexibleContexts, TypeFamilies #-}
+{-# language RankNTypes #-}
+module Reflex.Server.Warp where
+
+import Reflex hiding (Request, Response)
+import Reflex.Host.Basic (BasicGuestConstraints, BasicGuest, basicHostForever)
+import Reflex.Server.Wai
+
+import Control.Concurrent (forkIO)
+import Control.Monad (void)
+import Control.Monad.STM (atomically)
+import Network.Wai (Request, Response)
+import Network.Wai.Handler.Warp (Port, run)
+
+runAppForever
+  :: Port
+  -> (forall t m. BasicGuestConstraints t m => Event t Request -> BasicGuest t m (Event t Response))
+  -> IO ()
+runAppForever port network = do
+  waiSource <- atomically newWaiSource
+  void . forkIO $ basicHostForever (waiApplicationGuest waiSource network)
+  void . run port $ waiApplicationHost waiSource
